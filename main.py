3589936e70f00825f881cb15901696f77010a2a9
sytallax <sytallax@gmail.com>
2024-05-26 16:44:39 -0500

    refactor: update methods names to be more general

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(get_book_word_count(file_contents))
+        print(get_string_word_count(file_contents))
 
 
-def get_book_word_count(book: str) -> int:
-    """Calculates the word count for a given book"""
+def get_string_word_count(book: str) -> int:
+    """Calculates the word count for a given string"""
 
     return len(book.split())
 
