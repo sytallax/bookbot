b4dfd822ee4af7edf2d1d62a4be79fb7f6bc88f5
sytallax <sytallax@gmail.com>
2024-05-26 16:42:56 -0500

    feat: create method to calculate book word count

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        print(get_book_word_count(file_contents))
+
+
+def get_book_word_count(book: str) -> int:
+    """Calculates the word count for a given book"""
+
+    return len(book.split())
 
 
 main()
