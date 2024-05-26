ef6a954c1635b50bb3cd63856bd6e0d140d483b4
sytallax <sytallax@gmail.com>
2024-05-26 16:57:41 -0500

    feat: create method to count letters from book

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+from typing import Counter, Dict
+
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(get_book_word_count(file_contents))
+        print(get_book_letter_count(file_contents))
 
 
 def get_book_word_count(book: str) -> int:
     """Calculates the word count for a given book"""
-
     return len(book.split())
+
+
+def get_book_letter_count(book: str) -> Dict[str, int]:
+    """Calculates the count of each letter in a given book"""
+    return Counter(book.lower())
 
 
 main()
